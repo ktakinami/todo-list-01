36e2a4b8fe8a0f49b35eb223fa5e568840e488aa
Koji Takinami <88888540+ktakinami@users.noreply.github.com>
2022-06-12 20:55:04 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+let inputBox = document.getElementById('input-field')
+let addButton = document.getElementById('btn-add')
+let todoContainer = document.getElementById('list-container')
+
+addButton.addEventListener('click', function() {
+    let paragraph = document.createElement('p')
+    let breakLine = document.createElement('br')
+
+    paragraph.classList.add('paragraph')
+    paragraph.innerText = inputBox.value
+    todoContainer.appendChild(paragraph)
+    todoContainer.appendChild(breakLine)
+    inputBox.value = ''
+
+    paragraph.addEventListener('click', function() {
+        paragraph.style.textDecoration = 'line-through'
+    })
+
+    paragraph.addEventListener('dblclick', function() {
+        paragraph.style.textDecoration = 'none'
+    })
+       /*
+        paragraph.addEventListener('dblclick', function() {
+            todoContainer.removeChild(paragraph)
+    }) 
+        */
+})
